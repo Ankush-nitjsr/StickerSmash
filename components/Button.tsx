d7e682b821f9b7066a7b367a98262467295fe9b8
Ankush <ankushnitjsr@gmail.com>
2025-02-23 22:44:58 +0530

    feat: Update Button component
    Add theme prop to button component to improve UI

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,44 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { View, Text, StyleSheet, Pressable } from "react-native";
 
 type Props = {
   label: string;
+  theme?: "primary";
 };
 
-export default function Button({ label }: Props) {
+export default function Button({ label, theme }: Props) {
+  if (theme === "primary") {
+    return (
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 },
+        ]}
+      >
+        <Pressable
+          style={[styles.button, { backgroundColor: "#fff" }]}
+          onPress={() => alert("You pressed a button.")}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292e"
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: "#25292e" }]}>
+            {label}
+          </Text>
+        </Pressable>
+      </View>
+    );
+  }
   return (
     <View style={styles.buttonContainer}>
       <Pressable
         style={styles.button}
         onPress={() => alert("You pressed a button.")}
       >
-        <Text style={styles.buttonLabel}>Button</Text>
+        <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
   );
@@ -37,5 +64,8 @@ const styles = StyleSheet.create({
   buttonLabel: {
     color: "#fff",
     fontSize: 16,
+  },
+  buttonIcon: {
+    paddingRight: 8,
   },
 });
